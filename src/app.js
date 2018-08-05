79a4c46f3cbafc856569e791260c2e16f0a45c01
Kevin Hannan <kphannan@gmail.com>
2018-08-05 16:40:59 -0400

    force everthing to update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log( "Hello World ... from webpack");
+import React from 'react';
+// import { render } from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+// class App extends React.Component {
+//     render() {
+//         return <p>Hello React! </p>;
+//     }
+// }
+
+
+
+// render( <App>, document.getElementById( 'app' ));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,53 @@
-module.exports = {
-    entry: './src/app.js',
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         filename: './app.bundle.js'
+//     }
+// }
+
+var webpack = require('webpack');
+var path = require( 'path' );
+
+var BUILD_DIR = path.join( __dirname, 'dir' );
+var APP_DIR = path.join( __dirname, 'src' );
+
+var config = {
+    entry: APP_DIR + '/app.js',
     output: {
-        filename: './app.bundle.js'
+        path: BUILD_DIR,
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    { loader: 'sass-loader' }
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+        // loaders: [
+        //     {
+        //         test: /\.(js|jsx)$/,
+        //         // include: APP_DIR,
+        //         exclude: /node_modules/,
+        //         use: 'babel-loader'
+        //     }
+        // ]
     }
 }
+
+
+module.exports = config;
+
